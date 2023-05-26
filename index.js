9002fee598587858386df5845d9d61398d88f3ad
wewelski <f.roowell@gmail.com>
2023-05-26 14:32:00 +0800

    added nodemon

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.use((request, response) => {
 // line 6 is an html response for the client to see
 // app.use method accepts two params: request & response, which in this case response is sent back
 
+app.get('/r/:mypage',(request,response) => {
+  const { mypage } = request.params;
+  response.send(`Ayt, you are on the S{mypage} section.`);
+})
+
+app.get('/',(request,response) => {
+  response.send('You are now in the homepage.');
+  console.log('Well done!');
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
